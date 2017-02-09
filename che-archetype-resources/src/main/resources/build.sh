077e9afd89bcc59e7f996b571a914e5c377f993d
Mihail Kuznyetsov <mkuznyetsov@codenvy.com>
2017-02-09 10:55:13 +0200

    Fix licenses. Remove dos2unix execution

--- che-archetype-resources/src/main/resources/build.sh
+++ che-archetype-resources/src/main/resources/build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 #
-# Copyright (c) 2012-2017 Codenvy, S.A.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
-#
-# Contributors:
-#   Codenvy, S.A. - initial API and implementation
 #
 
 # See: https://sipb.mit.edu/doc/safe-shell/
@@ -16,11 +12,6 @@ set -u
 
 . ./build.include
 init "$@"
-
-if has_docker_for_windows_client; then
-# Convert files to Unix format with dos2unix
-/usr/bin/find . -type f -exec dos2unix {} \;
-fi
 
 #TODO detect m2 repo from settings.xml
 docker_exec run -it --rm --name build-che \
