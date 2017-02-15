68b437a84021e836531821490085a042c3e5a014
Mihail Kuznyetsov <mkuznyetsov@codenvy.com>
2017-02-15 16:59:09 +0200

    Detect installed Maven, before evaluating local repo path

--- che-archetype-resources/src/main/resources/build.sh
+++ che-archetype-resources/src/main/resources/build.sh
@@ -13,8 +13,15 @@ set -u
 . ./build.include
 init "$@"
 
-MVN_REPO=$(mvn help:evaluate -Dexpression=settings.localRepository | grep -E '^([a-zA-Z]:|/)')
-MVN_REPO=${MVN_REPO:=${HOME}/.m2/repository}
+DEFAULT_MVN_REPO=${HOME}/.m2/repository
+
+if command -v mvn >/dev/null 2>&1; then
+  MVN_REPO=$(mvn help:evaluate -Dexpression=settings.localRepository | grep -E '^([a-zA-Z]:|/)')
+  echo "Detected Maven, mounting local repo at ${MVN_REPO}"
+else
+  MVN_REPO=${DEFAULT_MVN_REPO}
+  echo "Maven not detected, mounting local repo at default path: ${DEFAULT_MVN_REPO}";
+fi
 
 docker_exec run -it --rm --name build-che       \
        -v "$MVN_REPO:/home/user/.m2/repository" \
