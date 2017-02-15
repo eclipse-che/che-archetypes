63130075b8e03fbdb9b84311d2760dec36c9c82a
Mihail Kuznyetsov <mkuznyetsov@codenvy.com>
2017-02-15 15:41:27 +0200

    Detect local maven repository for build script

--- che-archetype-resources/src/main/resources/build.sh
+++ che-archetype-resources/src/main/resources/build.sh
@@ -13,10 +13,12 @@ set -u
 . ./build.include
 init "$@"
 
-#TODO detect m2 repo from settings.xml
-docker_exec run -it --rm --name build-che \
-       -v "$HOME/.m2:/home/user/.m2"    \
-       -v "$PWD":/home/user/che-build   \
-       -w /home/user/che-build          \
-       codenvy/che-dev                  \
+MVN_REPO=$(mvn help:evaluate -Dexpression=settings.localRepository | grep -E '^([a-zA-Z]:|/)')
+MVN_REPO=${MVN_REPO:=${HOME}/.m2/repository}
+
+docker_exec run -it --rm --name build-che       \
+       -v "$MVN_REPO:/home/user/.m2/repository" \
+       -v "$PWD":/home/user/che-build           \
+       -w /home/user/che-build                  \
+       codenvy/che-dev                          \
        mvn clean install
