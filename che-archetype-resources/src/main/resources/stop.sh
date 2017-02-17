23f1b6a360194220985c42df0c10e37ef91253b0
Mihail Kuznyetsov <mkuznyetsov@codenvy.com>
2017-02-17 17:24:00 +0200

    Created Wizard archetype with Che & Codenvy assemblies

---
+++ che-archetype-resources/src/main/resources/stop.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+
+# See: https://sipb.mit.edu/doc/safe-shell/
+set -e
+set -u
+
+. ./build.include
+init "$@"
+
+locate_assembly_folder
+
+USAGE="Specify assembly to run (--che or --codenvy)"
+
+if [ $# = 0 ]; then
+    echo $USAGE
+    exit 1
+fi
+
+case $1 in
+    --che )        CLI_IMAGE=eclipse/che-cli:nightly
+                   DATA_MOUNT=$HOME/.che/sample/data
+                   ASSEMBLY_MOUNT=$PWD/assembly-che/assembly-main/target/eclipse-che-0.1-SNAPSHOT/eclipse-che-0.1-SNAPSHOT
+                   ;;
+    --codenvy )    CLI_IMAGE=codenvy/cli:nightly
+                   DATA_MOUNT=$HOME/.codenvy/sample/data
+                   ASSEMBLY_MOUNT=$PWD/assembly-codenvy/assembly-main/target/codenvy-onpremises-0.1-SNAPSHOT
+                   ;;
+    * )            echo $USAGE
+                   exit 1
+esac
+
+echo $CLI_IMAGE
+
+#TODO detect version of assembly. detect version of che
+docker_exec run -it --rm  ${DOCKER_RUN_OPTIONS}  \
+         -v /var/run/docker.sock:/var/run/docker.sock \
+         -v "$DATA_MOUNT:/data" \
+         -v "$ASSEMBLY_MOUNT:/assembly" \
+         $CLI_IMAGE stop
