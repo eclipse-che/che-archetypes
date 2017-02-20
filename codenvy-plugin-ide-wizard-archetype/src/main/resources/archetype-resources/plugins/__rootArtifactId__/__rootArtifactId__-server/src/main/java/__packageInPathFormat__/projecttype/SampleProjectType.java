6ce27275b14d470306ef87893aae8d339929d3fd
Michail Kuznetsov <mkuznyetsov@codenvy.com>
2017-02-20 12:25:46 +0200

    Add Che & Codenvy assembly to Plugin Wizard (#22)
    * Created Wizard archetype with Che & Codenvy assemblies
    
    * small fix
    
    * fixup! small fix
    
    * fixup! fixup! small fix

--- codenvy-plugin-ide-wizard-archetype/src/main/resources/archetype-resources/plugins/__rootArtifactId__/__rootArtifactId__-server/src/main/java/__packageInPathFormat__/projecttype/SampleProjectType.java
+++ codenvy-plugin-ide-wizard-archetype/src/main/resources/archetype-resources/plugins/__rootArtifactId__/__rootArtifactId__-server/src/main/java/__packageInPathFormat__/projecttype/SampleProjectType.java
@@ -2,14 +2,11 @@
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
 /*******************************************************************************
- * Copyright (c) 2012-2017 Codenvy, S.A.
+ * Copyright (c) 2012-2017
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Codenvy, S.A. - initial API and implementation
  *******************************************************************************/
 package ${package}.projecttype;
 
