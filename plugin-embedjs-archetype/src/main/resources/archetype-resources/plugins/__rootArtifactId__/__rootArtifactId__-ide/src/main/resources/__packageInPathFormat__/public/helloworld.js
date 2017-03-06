a48331fd4a03ff2ec7c0b2ac2cf3745c0709c735
Mihail Kuznyetsov <mkuznyetsov@codenvy.com>
2017-03-06 14:50:09 +0200

    Add Embedded Javascript Sample archetype

---
+++ plugin-embedjs-archetype/src/main/resources/archetype-resources/plugins/__rootArtifactId__/__rootArtifactId__-ide/src/main/resources/__packageInPathFormat__/public/helloworld.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2012-2017
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+function HelloWorld(
+    element, contents) {
+    element.textContent = contents;
+};
+
