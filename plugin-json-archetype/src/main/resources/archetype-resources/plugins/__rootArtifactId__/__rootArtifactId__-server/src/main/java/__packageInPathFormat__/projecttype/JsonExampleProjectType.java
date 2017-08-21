91d229a7a010f53f7759821f75aa81da6216e8c2
Mihail Kuznyetsov <mkuznets@redhat.com>
2017-08-21 11:42:41 +0300

    fixup! Apply new formatter for archetypes

--- plugin-json-archetype/src/main/resources/archetype-resources/plugins/__rootArtifactId__/__rootArtifactId__-server/src/main/java/__packageInPathFormat__/projecttype/JsonExampleProjectType.java
+++ plugin-json-archetype/src/main/resources/archetype-resources/plugins/__rootArtifactId__/__rootArtifactId__-server/src/main/java/__packageInPathFormat__/projecttype/JsonExampleProjectType.java
@@ -1,36 +1,27 @@
-/*******************************************************************************
- * Copyright (c) 2012-2017 Codenvy, S.A.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Codenvy, S.A. - initial API and implementation
- *******************************************************************************/
+/*
+ * Copyright (c) 2012-2017 All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package ${package}.projecttype;
-
-import com.google.inject.Inject;
-
-import org.eclipse.che.api.project.server.type.ProjectTypeDef;
-import ${package}.shared.Constants;
 
 import static ${package}.shared.Constants.JSON_EXAMPLE_LANG;
 import static ${package}.shared.Constants.JSON_EXAMPLE_PROJECT_TYPE_ID;
 import static ${package}.shared.Constants.LANGUAGE;
 
-/**
- * The JSON Example project type.
- */
+import com.google.inject.Inject;
+import ${package}.shared.Constants;
+import org.eclipse.che.api.project.server.type.ProjectTypeDef;
+
+/** The JSON Example project type. */
 public class JsonExampleProjectType extends ProjectTypeDef {
 
-    /**
-     * Constructor for the JSON example project type.
-     */
-    @Inject
-    public JsonExampleProjectType() {
-        super(JSON_EXAMPLE_PROJECT_TYPE_ID, JSON_EXAMPLE_PROJECT_TYPE_ID, true, false);
-        addConstantDefinition(LANGUAGE, LANGUAGE, JSON_EXAMPLE_LANG);
-        addVariableDefinition(Constants.JSON_EXAMPLE_SCHEMA_REF_ATTRIBUTE, "Referenced base schema", /*required*/ true);
-    }
+  /** Constructor for the JSON example project type. */
+  @Inject
+  public JsonExampleProjectType() {
+    super(JSON_EXAMPLE_PROJECT_TYPE_ID, JSON_EXAMPLE_PROJECT_TYPE_ID, true, false);
+    addConstantDefinition(LANGUAGE, LANGUAGE, JSON_EXAMPLE_LANG);
+    addVariableDefinition(
+        Constants.JSON_EXAMPLE_SCHEMA_REF_ATTRIBUTE, "Referenced base schema", /*required*/ true);
+  }
 }
